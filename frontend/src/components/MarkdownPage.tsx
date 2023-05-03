69bf948ccef2dc576a3d772f6b2e19bc45844e25
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-03 19:10:12 +0200

    added some event handling functions and waiting for the user data and server to be up and running

--- frontend/src/components/MarkdownPage.tsx
+++ frontend/src/components/MarkdownPage.tsx
@@ -5,12 +5,30 @@ import NotesDisplayComponent from  './pagecomponents/NotesDisplayComponent';
 
 export default function MarkdownPage(): JSX.Element {
 
+  //TODO: Import the users information after the login from the context API
+  //TODO: use axios to POST, DELETE, PATCH
+ const handlePost = (event) => {
+console.log(event)
+ }
+
+ const handlePatch = (event) => {
+console.log(event)
+ }
+ const handleDelete = (event) => {
+  console.log(event)
+ }
+
+
+
+
+
+
   return (
     <>
      <div className='button-container'>
-          <button type="submit" className="submit-post-button">Post</button>
-          <button type="submit" className="submit-edit-button">Edit</button>
-          <button type="submit" className="submit-delete-button">Delete</button>
+          <button type="submit" className="submit-post-button" onClick={ (e) => handlePost(e)}>Post</button>
+          <button type="submit" className="submit-edit-button" onClick={(e) => handlePatch(e)}>Edit</button>
+          <button type="submit" className="submit-delete-button" onClick={(e) => handleDelete(e)}>Delete</button>
         </div>
     <h1 className='page-header'>MarkdownPage</h1>
     <div className='flex-row'>
