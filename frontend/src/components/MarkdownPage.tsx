67cdfeba72f13e723a2135fdc75dba88a835ac69
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-03 19:03:42 +0200

    added three buttons

--- frontend/src/components/MarkdownPage.tsx
+++ frontend/src/components/MarkdownPage.tsx
@@ -7,11 +7,15 @@ export default function MarkdownPage(): JSX.Element {
 
   return (
     <>
+     <div className='button-container'>
+          <button type="submit" className="submit-post-button">Post</button>
+          <button type="submit" className="submit-edit-button">Edit</button>
+          <button type="submit" className="submit-delete-button">Delete</button>
+        </div>
     <h1 className='page-header'>MarkdownPage</h1>
     <div className='flex-row'>
     <NotesDisplayComponent/>
     <EditorComponent/>
-  
     </div>
       </>
   );
