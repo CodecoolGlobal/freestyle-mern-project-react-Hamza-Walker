f1edae128efb2c4c56a7f80205065c4c87b8d336
ABitOfAByte <marcus.migotti@gmail.com>
2023-05-04 13:51:01 +0200

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react"
 import MarkdownPage from "./components/MarkdownPage"
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google"
 import jwt_decode from "jwt-decode"
+import LoginPage from "./components/LoginPage"
 
 const clientId = "161332176689-u4kmfe25ddfu9na2ick4f9b5d2990cir.apps.googleusercontent.com"
 
@@ -13,27 +14,9 @@ export default function App() {
 		"# A demo of `react-markdown`\n\n`react-markdown` is a markdown component for React.\n\n👉 Changes are re-rendered as you type.\n\n👈 Try writing some markdown on the left.\n\n## Overview\n\nA component by [Espen Hovlandsdal](https://espen.codes/)"
 	)
 
-	let toRender = ""
-
-	if (!user) {
-		toRender = (
-			<GoogleLogin
-				onSuccess={response => {
-					const user = jwt_decode(response.credential)
-					setUser(user)
-					console.log(jwt_decode(response.credential))
-				}}
-				text="continue_with"
-				cancel_on_tap_outside={true}
-			/>
-		)
-	} else {
-		toRender = <MarkdownPage />
-	}
-
 	return (
-		<AppContext.Provider value={{ user, text, setText }}>
-			<GoogleOAuthProvider clientId={clientId}>{toRender}</GoogleOAuthProvider>
+		<AppContext.Provider value={{ user, setUser, text, setText }}>
+			<GoogleOAuthProvider clientId={clientId}>{!user ? <LoginPage /> : <MarkdownPage />}</GoogleOAuthProvider>
 		</AppContext.Provider>
 	)
 }
