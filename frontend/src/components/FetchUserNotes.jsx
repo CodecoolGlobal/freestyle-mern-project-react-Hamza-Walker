a958b46797835a1fff0c4dfc9e8123b77d549e52
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-05 06:44:15 +0200

    slowly progressing , my coffee is too hot to drink

--- frontend/src/components/FetchUserNotes.jsx
+++ frontend/src/components/FetchUserNotes.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { AppContext } from "../App"
+import '../assets/css/fetch-user-notes.css'
 
 export default function FetchUserNotes() {
   const [notes, setNotes] = useState([]);
@@ -20,14 +21,40 @@ export default function FetchUserNotes() {
     fetchNotes();
   }, [user]);
 
+  const handleEditNote = (noteId) => {
+    // implement edit note logic here
+  }
+
+  const handleDeleteNote = (noteId) => {
+    // implement delete note logic here
+  }
+
   return (
     <div>
       {notes.map((note) => (
-        <div key={note._id}>
-          <h2>{note.title}</h2>
-          <p>{note.content}</p>
+        <div key={note._id} className="fetch-user-note" onClick={() => handleEditNote(note._id)}>
+          <div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+          <div>
+            <button onClick={(e) => {
+              e.stopPropagation();
+              handleEditNote(note._id);
+            }}>Edit</button>
+            <button onClick={(e) => {
+              e.stopPropagation();
+              handleDeleteNote(note._id);
+            }}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
   );
 }
+
+
+
+
+
+
