2128dadc9918c81daedfc96a806f9bd74d1de868
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-05-12 06:32:11 +0200

    forgot to include the link in the html file so that is finally resolved and the style is looking awesome and all the alements are aligning properly

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { AppContext } from '../../main';
 
-const NavigationBar = (user) => {
+const NavigationBar = () => {
+  const {user} = useContext(AppContext);
   return (
-    <Navbar color="light" light expand="md">
+    <Navbar color="dark" dark expand="md">
       <NavbarBrand href="/">My Notes App</NavbarBrand>
       <Nav className="ml-auto" navbar>
         <NavItem>
-          <NavLink href="#">Display All Notes</NavLink>
+          <NavLink href="#" className="text-light">Display All Notes</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="#">Create New Note</NavLink>
+          <NavLink href="#" className="text-light">Create New Note</NavLink>
         </NavItem>
-        <NavItem>
-          <img src={user.iconURL} alt="User Profile" />
+        <NavItem className="ml-3">
+          <img src={user.iconURL} alt="User Avatar" className="rounded-circle" width="50" height="50" />
         </NavItem>
       </Nav>
     </Navbar>
