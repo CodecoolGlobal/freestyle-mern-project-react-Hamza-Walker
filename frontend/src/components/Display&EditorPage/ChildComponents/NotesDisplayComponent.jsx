4b14681a92a26c7001dc9617f41366798710627c
Lilpeanutz <shengkaixia@gmx.at>
2023-05-22 12:51:17 +0200

    save commit

--- frontend/src/components/Display&EditorPage/ChildComponents/NotesDisplayComponent.jsx
+++ frontend/src/components/Display&EditorPage/ChildComponents/NotesDisplayComponent.jsx
@@ -1,8 +1,39 @@
-import React, { useContext } from "react"
-import { AppContext } from "../../../main"
-import { marked } from "marked"
+// import React, { useContext } from "react"
+// import { AppContext } from "../../../main"
+// import { marked } from "marked"
+
+// export default function NotesDisplayComponent() {
+// 	const { text } = useContext(AppContext)
+// 	return <div className="dangerous-dev" dangerouslySetInnerHTML={{ __html: marked(text) }}></div>
+// }
+
+
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../main";
+import { Button } from "reactstrap";
+import ReactMarkdown from "react-markdown";
+import Speech from "react-speech";
 
 export default function NotesDisplayComponent() {
-	const { text } = useContext(AppContext)
-	return <div className="dangerous-dev" dangerouslySetInnerHTML={{ __html: marked(text) }}></div>
+  const { selectedNote } = useContext(AppContext);
+  const [speechSynthesisSupported, setSpeechSynthesisSupported] = useState(false);
+
+  useEffect(() => {
+    const synthesisSupported = "speechSynthesis" in window;
+    setSpeechSynthesisSupported(synthesisSupported);
+  }, []);
+
+  const handleReadAloud = () => {
+    const utterance = new SpeechSynthesisUtterance(selectedNote?.content);
+    window.speechSynthesis.speak(utterance);
+  };
+
+  return (
+    <div>
+      <ReactMarkdown>{selectedNote?.content}</ReactMarkdown>
+      {speechSynthesisSupported && (
+        <Button onClick={handleReadAloud}>Read Aloud</Button>
+      )}
+    </div>
+  );
 }
